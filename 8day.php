27388f0a12fa8b638396f43e781d89f9533567c7
tanayagaikwad2004 <109025925+tanayagaikwad2004@users.noreply.github.com>
2022-07-16 14:36:42 +0530

    Create 8day.php

---
+++ 8day.php
@@ -0,0 +1,13 @@
+<?php 
+
+// day 1 php installation
+// day 2 variable declaration
+// day 3 for loop tables 
+// day 4 arrays and git 
+// day 5 while loop functions include require
+// day 6 how to create the files
+// day 7 rss feeds
+// day 8 database connection
+// day 9 fetch values from the database
+
+?>
