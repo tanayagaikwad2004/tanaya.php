a3df4e951fdd22c05e0e1099aad71ba0043616f0
tanayagaikwad2004 <109025925+tanayagaikwad2004@users.noreply.github.com>
2022-07-16 14:08:21 +0530

    Create includes.php

---
+++ includes.php
@@ -0,0 +1,7 @@
+<?php
+ include("functions.php"); 
+ // require("functions.php"); require and include have same functionality only exception is 
+ // if file is not present below lines of code are not executed
+
+echo "hello world";
+?>
